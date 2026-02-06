fa2a1a9c64a44c1c522f137cb821b65d5fc8c79c
akshay <akshaypskrishna@gmail.com>
2026-02-06 16:49:41 +0530

    feat: connect workspace creation and retrieval
    Users associated with workspaces not yet implemented, requires backend support.
    Need to integrate updation, deletion as well.
    Currently just setting created_by as user_1, need user auth before proper integration.

--- soul.prism/src/app/layout.tsx
+++ soul.prism/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./styles/theme.css";
+import QueryProvider from "./providers/QueryProviders";
+import { Toaster } from "@/components/toast/toaster"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen`}
       >
-        {children}
+        <QueryProvider>
+          {children}
+          <Toaster />
+        </QueryProvider>
       </body>
     </html>
   );
