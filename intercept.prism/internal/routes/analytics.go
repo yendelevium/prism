b9f8cd9b88a4f56ccfa5cf637647f2cbd0686e50
Anaswara K <anaswarakorangot@gmail.com>
2026-02-11 11:24:04 +0530

    Delete analytics.go file completely

--- intercept.prism/internal/routes/analytics.go
+++
@@ -1,5 +0,0 @@
-package routes
-
-// This file previously contained Service Map implementation
-// which has been migrated to soul.prism (TypeScript/Next.js)
-// The Service Map is now available at soul.prism/src/components/bottompanel/servicemap/
